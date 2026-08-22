5d4e0621cfce0496fa5fa04755cd2e21eb881356
hasnat-456 <hasnatmuhammad718@gmail.com>
2026-08-22 15:33:20 +0500

    Clean up temporary scripts and extraction folders

--- fix_footer.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('front/about.html', 'utf8');
-
-const regex = /products\.html<svg[^>]*>category=/g;
-code = code.replace(regex, '/products?category=');
-
-fs.writeFileSync('front/about.html', code, 'utf8');
-console.log("Fixed corrupted footer links in about.html");
